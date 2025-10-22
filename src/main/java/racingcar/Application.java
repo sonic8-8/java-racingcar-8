f2a857a8f6fb3e0bcbdaa37336f0c0385e6c8a7d
이승환 <seaung13@gmail.com>
2025-10-22 17:52:16 +0900

    feat(Application): 프로그램 실행을 위한 의존성 주입

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,21 @@
 package racingcar;
 
+import racingcar.io.InputHandler;
+import racingcar.io.OutputHandler;
+import racingcar.model.RoundManager;
+import racingcar.util.CarNameValidator;
+import racingcar.util.UserInputCarNameParser;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputHandler inputHandler = new InputHandler();
+        OutputHandler outputHandler = new OutputHandler();
+        RoundManager roundManager = new RoundManager();
+        CarNameValidator carNameValidator = new CarNameValidator();
+
+        UserInputCarNameParser parser = new UserInputCarNameParser(carNameValidator);
+
+        RacingCar racingCar = new RacingCar(inputHandler, outputHandler, parser, roundManager);
+        racingCar.run();
     }
 }
